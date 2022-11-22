c7dbfc7845375a0abfa2804e3ddf1ffac2633364
Frank <delanofofe@gmail.com>
2022-11-22 04:28:42 +0100

    commit from monaco

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('/test', function(Request $request){
+    return "Authenticated";
 });
