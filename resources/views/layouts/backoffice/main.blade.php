c64f006e85ae9c9fe8905f01937c838602aa3447
imagic <aleximagic2020@gmail.com>
2022-11-25 13:25:01 +0100

    imagic

---
+++ resources/views/layouts/backoffice/main.blade.php
@@ -0,0 +1,48 @@
+<!doctype html>
+<html class="no-js " lang="en">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=Edge">
+<meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+<meta name="description" content="Responsive Bootstrap 4 and web Application ui kit.">
+<title>:: KiPART Admin :: Home</title>
+<link rel="icon" href="favicon.ico" type="image/x-icon"> <!-- Favicon-->
+<link rel="stylesheet" href="assets/plugins/bootstrap/css/bootstrap.min.css">
+<link rel="stylesheet" href="assets/plugins/jvectormap/jquery-jvectormap-2.0.3.min.css"/>
+<link rel="stylesheet" href="assets/plugins/charts-c3/plugin.css"/>
+
+<link rel="stylesheet" href="assets/plugins/morrisjs/morris.min.css" />
+
+<!-- Custom Css -->
+<link rel="stylesheet" href="assets/css/style.min.css">
+</head>
+
+<body class="theme-blush">
+<!-- Page Loader -->
+<div class="page-loader-wrapper">
+    <div class="loader">
+        <div class="m-t-30"><img class="zmdi-hc-spin" src="assets/images/loader.svg" width="48" height="48" alt="Aero"></div>
+        <p>Please wait...</p>
+    </div>
+</div>
+<!-- Right Icon menu Sidebar -->
+
+<!--end icon-->
+
+<!-- Right and left Sidebar -->
+
+<!--end sider-->
+@yield('contennt')
+
+<!-- Jquery Core Js -->
+<script src="assets/bundles/libscripts.bundle.js"></script> <!-- Lib Scripts Plugin Js ( jquery.v3.2.1, Bootstrap4 js) -->
+<script src="assets/bundles/vendorscripts.bundle.js"></script> <!-- slimscroll, waves Scripts Plugin Js -->
+
+<script src="assets/bundles/jvectormap.bundle.js"></script> <!-- JVectorMap Plugin Js -->
+<script src="assets/bundles/sparkline.bundle.js"></script> <!-- Sparkline Plugin Js -->
+<script src="assets/bundles/c3.bundle.js"></script>
+
+<script src="assets/bundles/mainscripts.bundle.js"></script>
+<script src="assets/js/pages/index.js"></script>
+</body>
+</html>
