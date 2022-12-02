085b630ca9539ade5b50e29a714b356ce149899d
level237 <Bramslevel129@gmail.com>
2022-12-02 10:33:04 -0500

    Multi guard:Redirect in dashboard user

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -24,7 +24,7 @@ class RedirectIfAuthenticated
         foreach ($guards as $guard) {
             if (Auth::guard($guard)->check()) {
 
-                return redirect()->route('homepage');
+                return redirect()->route('user.dashboard');
                 //return redirect(RouteServiceProvider::HOME);
             }
         }
