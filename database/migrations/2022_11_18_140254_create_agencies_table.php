a1463224f3ebaa6805b37f1abd566b9a2ce2c5b4
level237 <Bramslevel129@gmail.com>
2022-12-05 23:25:11 -0500

    update

--- database/migrations/2022_11_18_140254_create_agencies_table.php
+++ database/migrations/2022_11_18_140254_create_agencies_table.php
@@ -19,7 +19,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('headquarters');
             $table->string('logo');
-            $table->string('email')->unique()->nullable();
+            $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('phone_number');
