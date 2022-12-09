4455c74679b9508e070dfb5162d106995a0eae48
level237 <Bramslevel129@gmail.com>
2022-12-09 09:23:36 -0500

    Api Kipart:upload image

--- resources/views/admin/agencies/index.blade.php
+++ resources/views/admin/agencies/index.blade.php
@@ -49,7 +49,7 @@
                                     @foreach ($datas as $agencies)
                                         @forelse ($agencies as $agency)
                                             <tr>
-                                                <td><img src="{{ Storage::url($agency->logo) }}" width="48" alt="Product img"></td>
+                                                <td><img src="{{ Storage::getMimeType($agency->logo) }}" width="48" alt="Product img"></td>
                                                 <td><h5>{{ $agency->name }}</h5></td>
                                                 <td>
                                                     <a href="{{ route('admin.agencies.edit',$agency->id) }}" class="btn btn-default waves-effect waves-float btn-sm waves-green"><i class="zmdi zmdi-edit"></i></a>
