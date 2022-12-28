3e8ba5ba72b3459b5624c4128972cd65a9dff8bf
level237 <Bramslevel129@gmail.com>
2022-12-28 15:49:30 +0100

    update

--- app/Http/Controllers/Api/SearchController.php
+++ app/Http/Controllers/Api/SearchController.php
@@ -67,7 +67,6 @@ class SearchController extends Controller
 
                 ->where('travel.agency_id','=',$id)
                 ->where('paths.departure','=',$request->departure)
-                ->where('schedules.hours','>',$request->hours)
                 ->where('paths.arrival','=',$request->arrival)
                 ->where('travel.date','=',$request->dateDeparture)
                 ->get();
