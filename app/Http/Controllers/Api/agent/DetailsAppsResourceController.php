2998b83f83386e0051c3358271a0e184afe31529
level237 <Bramslevel129@gmail.com>
2023-01-07 11:30:22 +0100

    update:count resource

--- app/Http/Controllers/Api/agent/DetailsAppsResourceController.php
+++ app/Http/Controllers/Api/agent/DetailsAppsResourceController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api\agent;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Controller;
+use App\Models\Passenger;
 use Illuminate\Support\Facades\Auth;
 
 class DetailsAppsResourceController extends Controller
@@ -18,7 +19,9 @@ class DetailsAppsResourceController extends Controller
                     ->where('agencies.id','=',Auth::guard('api-agent')->user()->id)
                     ->count();
 
+        $ticket=Passenger::where('isCheckPayment','=',1)->count();
 
-        return response()->json(['numberOfBus'=>$countBus]);
+
+        return response()->json(['numberOfBus'=>$countBus,'ticket'=>$ticket]);
     }
 }
