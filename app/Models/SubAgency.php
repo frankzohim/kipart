9e1eae7091fb31f6c49b7e9face15bb64edce7a8
level237 <Bramslevel129@gmail.com>
2023-01-09 10:38:18 +0100

    subagencies init

---
+++ app/Models/SubAgency.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class SubAgency extends Model
+{
+    use HasFactory;
+}
