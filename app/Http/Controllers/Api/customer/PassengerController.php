f157d0da5d87fa7e070a5176335b757827408511
Level237 <Bramslevel129@gmail.com>
2023-02-10 18:43:54 +0100

    update

--- app/Http/Controllers/Api/customer/PassengerController.php
+++ app/Http/Controllers/Api/customer/PassengerController.php
@@ -65,48 +65,9 @@ class PassengerController extends Controller
         $listPlaceAvailable=array_values($listPlace);
 
 
-        for($i=0;$i<count($PassengerData['passengers']);$i++){
-            array_push($listPlacePassengers,$listPlaceAvailable[$i]);
-        }
-        if($travel_found){
-
-            $payment=Payment::create([
-                'user_id' =>Auth::guard('api')->user()->id,
-                'travel_id' =>$travel_id,
-                'means_of_payment'=>'visa card'
-
-            ]);
-
-            if(count($listPlaceAvailable)==0){
-                return response()->json(['message'=>"Toutes les places de ce bus ont deja été reservé"],200);
-            }
-            else{
-                foreach($PassengerData['passengers'] as $key => $value){
 
 
-                    $passengerModel=new Passenger;
-                    $passengerModel->cni=$value['cni'];
-                    $passengerModel->name=$value['name'];
-                    $passengerModel->type=$value['type'];
-                    $passengerModel->telephone=$value['telephone'];
-                    $passengerModel->seatNumber=$listPlacePassengers[$key];
-                    $passengerModel->isCheckPayment=0;
-                    $passengerModel->payment_id=$payment->id;
-                    $passengerModel->travel_id=$travel_id;
-                    $passengerModel->save();
-                }
-
-
-
-            return response()->json(['message'=>"Passager(s) ajouté avec success",'payment_id'=>$payment->id],201);
-            }
-
-
-        }else{
-            return response()->json(['message'=>"voyage non trouvé"],404);
-        }
-
-        return response()->json([$listPlaceAvailable,$placeBusy]);
+        return $PassengerData;
     }
 
 
--- database/migrations/2023_01_11_070743_create_tickets_table.php
+++ database/migrations/2023_01_11_070743_create_tickets_table.php
@@ -39,6 +39,8 @@ return new class extends Migration
                 ->constrained()
                 ->restrictOnUpdate()
                 ->restrictOnDelete();
+
+            $table->boolean('type')->nullable();
             $table->timestamps();
         });
     }
